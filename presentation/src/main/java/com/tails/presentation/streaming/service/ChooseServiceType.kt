80c33a28e3d2733bf424c455480c17d0102fabbc
Sungyong Son <space010127@gmail.com>
2019-05-01 12:03:50 +0900

    [Update] ChooseServiceType

--- presentation/src/main/java/com/tails/presentation/streaming/service/ChooseServiceType.kt
+++ presentation/src/main/java/com/tails/presentation/streaming/service/ChooseServiceType.kt
@@ -9,7 +9,7 @@ import androidx.work.WorkerParameters
 class ChooseServiceType(context: Context, workerParameters: WorkerParameters) :
     Worker(context, workerParameters) {
     override fun doWork(): Result {
-        applicationContext.stopService(Intent(applicationContext, ObserveTaskRemovedService::class.java))
+        //applicationContext.stopService(Intent(applicationContext, ObserveTaskRemovedService::class.java))
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             applicationContext.startForegroundService(Intent(applicationContext, ObserveTaskRemovedService::class.java))
         } else {
