917d9bc5c12f084e5430e207d2b3dceafbca4567
Sungyong Son <space010127@gmail.com>
2019-05-12 23:41:21 +0900

    no igonore SearchConfig

---
+++ data/src/main/java/com/tails/data/remote/search/SearchConfig.kt
@@ -0,0 +1,10 @@
+package com.tails.data.remote.search
+
+object SearchConfig {
+    internal const val YOUTUBE_SEARCH_LIST_TYPE = "video"
+    internal const val YOUTUBE_SEARCH_LIST_PART = "id,snippet"
+    internal const val YOUTUBE_SEARCH_LIST_FIELDS = "nextPageToken,items(id/videoId)"
+
+    internal const val YOUTUBE_LANGUAGE_KEY = "hl"
+    internal const val YOUTUBE_MAX_RESULTS = 50L
+}
